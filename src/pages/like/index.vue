20f3dce15b1473868dc7454c3a386fd26cb43296
jinsongding <jinsongding@sohu-inc.com>
2020-08-02 22:04:22 +0800

    点赞特效

---
+++ src/pages/like/index.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <like-fx :count="count"></like-fx>
+        <button class="mini-btn" type="warn" size="mini" @tap="clickHandler">{{count}}</button>
+    </div>
+</template>
+<script>
+import likeFx from './components/likeFx/index'
+
+export default {
+    components: {
+        likeFx
+    },
+    data() {
+        return {
+            count: 0 
+        }
+    },
+    mounted() {
+        // setInterval(() => {
+        //     this.count++
+        // }, 1000)
+    },
+    methods: {
+        clickHandler() {
+            this.count++
+        }
+    }
+}
+</script>
+<style scoped>
+.mini-btn{
+  position: absolute;
+  right: 15px;
+  bottom: 25px
+}
+</style>
