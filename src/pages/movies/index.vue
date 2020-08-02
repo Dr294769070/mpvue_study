b8cdd06ce08ee2335f7711267ba3728389adcb73
jinsongding <jinsongding@sohu-inc.com>
2020-08-02 17:50:43 +0800

    增加flyio插件

--- src/pages/movies/index.vue
+++ src/pages/movies/index.vue
@@ -4,11 +4,17 @@
     </div>
 </template>
 <script>
+import Fly from 'flyio/dist/npm/wx'
+
 export default {
     data() {
         return {
             // 
         }
+    },
+    created() {
+        const fly = new Fly
+        fly.get('http://t.yushu.im/v2/movie/top250').then((res) => {})
     }
 }
 </script>
